886d5bb7403f5dbe9e0a77cf073a86b0cfabbef8
Ploffin <plofftag@gmail.com>
2017-10-09 07:26:17 +0200

    movement functions for balls

--- engine/src/OctfElements.cpp
+++ engine/src/OctfElements.cpp
@@ -1,12 +1,12 @@
 #include "OctfElements.hpp"
 
-class b2World;
 class b2Body;
 
 OctfElement::OctfElement() { }
 OctfElement::~OctfElement() { }
 
 void OctfElement::setBody(b2Body* toSet) { _body = toSet; }
+b2Body* OctfElement::getBody() { return _body; }
 
 OctfElementDynamic::OctfElementDynamic() { }
 OctfElementDynamic::~OctfElementDynamic() { }
